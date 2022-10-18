f8146e3723cc5715755ae8b328e46162c156e6a1
hafeezmdx <97750829+hafeezmdx@users.noreply.github.com>
2022-10-18 16:50:51 +0100

    Update DRAFT_5.ino

--- DRAFT_5.ino
+++ DRAFT_5.ino
@@ -2,22 +2,22 @@
 
 #define BUTTON_PIN 12     // the button and 10K pulldown are connected to 12
 int SENSOR_fsr = A0;     // the FSR and 10K pulldown are connected to a0
-int sensors;     // the analog reading from the FSR resistor divider
+int sensors =0;     // the analog reading from the FSR resistor divider
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
 
 // for your motor
-const int stepPin = 3; //Y.STEP
-const int dirPin = 6; // Y.DIR
+const int stepPin = 2; //Y.STEP
+const int dirPin = 5; // Y.DIR
 
 byte lastButtonState = LOW;
 byte stepperState = LOW;
 
-unsigned long debounceDuration = 50; // millis
+unsigned long debounceDuration = 5; // millis
 unsigned long lastTimeButtonStateChanged = 0;
 
 int stepCount = 0;  // number of steps the motor has taken
 
-enum State_enum {STOP, TOUCH_DETECTED, PROBE_FORWARD, PROBE_BACK};
+enum State_enum {STOP, FORWARD, PROBE_FORWARD, PROBE_BACK};
 
 uint8_t state = STOP;
 
@@ -37,10 +37,6 @@ void setup() {
 
 void state_machine_run() {
 
-  sensors = analogRead(SENSOR_fsr);
-  Serial.print("Analog reading = ");
-  Serial.print(sensors);     // the raw analog reading
-  delay(1000);
   switch (state)
   {
     case STOP:
@@ -51,44 +47,54 @@ void state_machine_run() {
           lastButtonState = buttonState;
           if (buttonState == LOW) {
             motors_forward();
-            state = TOUCH_DETECTED;
+            delay(3000);
+            state = FORWARD;
           }
         }
       }
       break;
 
-    case TOUCH_DETECTED:
-      if (sensors >= 0) {
-        motors_stop();
+    case FORWARD:
+    sensors = analogRead(SENSOR_fsr);
+    
+    Serial.print("Analog reading = ");
+    Serial.print(sensors);     // the raw analog reading
+    
+    delay(1000);
+      if (sensors > 0) {
+        motors_probe();
+        delay(3000);
         state = PROBE_FORWARD;
       }
       break;
 
     case PROBE_FORWARD:
 
-      digitalWrite(dirPin, LOW);
-      for (int x = 0; x < 100; x++) {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(500);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(500);
-        motors_back();
+    if (sensors < 500) {
+    Serial.println(" - unriped peach");
+        motors_stop();
+        state = PROBE_BACK;
+  } else if (sensors > 550) {
+    Serial.println(" - riped peach");
+        motors_stop();
         state = PROBE_BACK;
       }
       break;
 
     case PROBE_BACK:
-       digitalWrite(dirPin, HIGH);
-      for (int x = 0; x < 200; x++) {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(500);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(500);
+       {
+        digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
+  for (int x = 0; x < 50; x++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
         motors_back();
         state = STOP;
       }
       break;
   }
+}
 }
 void loop()
 {
@@ -100,7 +106,7 @@ void motors_forward()
   //code for driving forward
   stepperState = (stepperState == HIGH) ? LOW : HIGH;
   digitalWrite(dirPin, LOW); // Enables the motor to move in a particular direction
-  for (int x = 0; x < 200; x++) {
+  for (int x = 0; x < 1000; x++) {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(500);
     digitalWrite(stepPin, LOW);
@@ -110,7 +116,7 @@ void motors_forward()
 void motors_stop()
 {
   //code for stopping motor
-  digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
+  digitalWrite(dirPin, LOW); // Enables the motor to move in a particular direction
   for (int x = 0; x < 0; x++) {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(500);
@@ -120,18 +126,24 @@ void motors_stop()
 }
 void motors_probe()
 {
-  if (sensors > 740) {
-    Serial.println(" - ripped peach");
-  } else if (sensors < 750) {
-    Serial.println(" - unripped peach");
-  } 
-  delay(1000);
-
-
+  digitalWrite(dirPin, LOW);
+      for (int x = 0; x < 100; x++) {
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(500);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(500);
+      }
 }
+
+
 void motors_back()
 {
-  if (sensors >= 0) {
-    Serial.println(" - Moving back");
+  //code for turning back
+  digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
+  for (int x = 0; x < 20; x++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
   }
 }
